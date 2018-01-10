eddaab5f43a8614b037b6991dc585e7167e3864e
Guest User <Guest@Epicodus-7D.local>
2018-01-10 15:27:27 -0800

    added scripts

--- js/scripts.js
+++ js/scripts.js
@@ -17,10 +17,47 @@ var favoriteColor = $("#color").val();
 
 
 $(document).ready(function() {
-  $("input").click(function(event){
+
+
+
+  var d = new Date();
+
+  var month = d.getMonth()+1;
+  var day = d.getDate();
+
+  var output =
+      ((''+month).length<2 ? '0' : '') + month + '/' +
+      ((''+day).length<2 ? '0' : '') + day + '/' + d.getFullYear();
+
+  //alert(output);
+  $(".date").html(output);
+
+  $(".submit").click(function(event){
       event.preventDefault();
-      $(".form").hide();
-      $(".receiptArea").show();
+
+      var email = $("#email").val();
+      $(".email").text(email);
+
+      var name = $("#name").val();
+      $(".name").text(name);
+
+      var address1 = $("#address1").val();
+      $(".address1").text(address1);
+
+      var address2 = $("#address2").val();
+      $(".address2").text(address2);
+
+      var city = $("#city").val();
+      $(".city").text(city);
+
+      var state = $("#state").val();
+      $(".state").text(state);
+
+      var country = $("#country").val();
+      $(".country").text(country);
+
+      /*$(".form").hide();
+      $(".receiptArea").show();*/
   });
 
 });
