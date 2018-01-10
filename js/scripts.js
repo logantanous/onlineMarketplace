45554251dbbb1bcab0caa8775a82efa2d77651d2
Guest User <Guest@Epicodus-7D.local>
2018-01-10 14:08:54 -0800

    created a purchase form

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+//Select Boxes
+var beverage = $("#beverage").val();
+
+//Radio Buttons
+var flavor = $("input:radio[name=flavor]:checked").val();
+
+//Checkboxes
+//we will cover this
+
+//Date
+var dob = $("#born").val();
+
+//Color
+var favoriteColor = $("#color").val();
